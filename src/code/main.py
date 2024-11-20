db6d9008f7a461fd5e3a017a900ce929d8a6f686
benedekaibas <kaibas.benedek02@gmail.com>
2024-11-20 13:54:11 -0500

    interpreter changed

--- src/code/main.py
+++ src/code/main.py
@@ -13,6 +13,7 @@ class RemoveWhiteSpace():
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
 
+
     def open_file(self):
         """Opening files from input."""
         with open(self.file_path, "r", encoding = "utf-8") as file:
@@ -20,15 +21,18 @@ class RemoveWhiteSpace():
 
         return content
 
+
     def remove_whitespace(self, content: str):
         """Removing whitespace from a file."""
         return re.sub(r' +', ' ', content)
+
 
     def save_modified_files(self, content: str) -> str:
         """Save the modified file for the user."""
         with open(self.file_path, 'w', encoding = "utf-8") as file:
             file.write(content)
             console.print(f"File '{self.file_path}' has been processed and saved.")
+
 
     def not_valid_file(self) -> bool:
         """Check if file is valid and exists"""
