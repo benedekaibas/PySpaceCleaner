7c722f4b8cde2e318b0ccf4cca52da8a6a058514
kaibas01 <kaibas01@allegheny.edu>
2024-11-23 14:18:05 -0500

    import statement does not pass the linter

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,11 +1,19 @@
+"""Test functions for the main.py file."""
+
+import sys
+import os
 import unittest
 from unittest.mock import mock_open, patch
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from code.main import RemoveWhiteSpace
 
 class TestRemoveWhiteSpace(unittest.TestCase):
     """Test cases for the main.py file. """
     @patch("builtins.open", new_callable=mock_open, read_data="This is a test file.\n\nWith some whitespace.  \n")
     def test_open_file(self, mock_file):
+        """Test case for the open file function."""
         rws = RemoveWhiteSpace("dummy_path")
         content = rws.open_file()
         self.assertEqual(content, "This is a test file.\n\nWith some whitespace.  \n")
@@ -13,6 +21,7 @@ class TestRemoveWhiteSpace(unittest.TestCase):
 
 
     def test_remove_whitespace(self):
+        """Test case for the remove whitespace function."""
         rws = RemoveWhiteSpace("dummy_path")
         content = "This is a test file.\n\nWith some whitespace.  \n"
         expected_content = "This is a test file.\n\nWith some whitespace.\n"
@@ -22,6 +31,7 @@ class TestRemoveWhiteSpace(unittest.TestCase):
 
     @patch("builtins.open", new_callable=mock_open)
     def test_save_modified_files(self, mock_file):
+        """Test case to see if the saving of the modified files are working correctly."""
         rws = RemoveWhiteSpace("dummy_path")
         content = "This is a test file.\n\nWith some whitespace.\n"
         rws.save_modified_files(content)
