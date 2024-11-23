ecfae0df8a0fe5161fdbde1d43a94ed882b91688
kaibas01 <kaibas01@allegheny.edu>
2024-11-23 14:11:38 -0500

    test file finished

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,2 +1,32 @@
 import unittest
-from code import main
+from unittest.mock import mock_open, patch
+from code.main import RemoveWhiteSpace
+
+class TestRemoveWhiteSpace(unittest.TestCase):
+    """Test cases for the main.py file. """
+    @patch("builtins.open", new_callable=mock_open, read_data="This is a test file.\n\nWith some whitespace.  \n")
+    def test_open_file(self, mock_file):
+        rws = RemoveWhiteSpace("dummy_path")
+        content = rws.open_file()
+        self.assertEqual(content, "This is a test file.\n\nWith some whitespace.  \n")
+        mock_file.assert_called_once_with("dummy_path", "r", encoding="utf-8")
+
+
+    def test_remove_whitespace(self):
+        rws = RemoveWhiteSpace("dummy_path")
+        content = "This is a test file.\n\nWith some whitespace.  \n"
+        expected_content = "This is a test file.\n\nWith some whitespace.\n"
+        modified_content = rws.remove_whitespace(content)
+        self.assertEqual(modified_content, expected_content)
+
+
+    @patch("builtins.open", new_callable=mock_open)
+    def test_save_modified_files(self, mock_file):
+        rws = RemoveWhiteSpace("dummy_path")
+        content = "This is a test file.\n\nWith some whitespace.\n"
+        rws.save_modified_files(content)
+        mock_file().write.assert_called_once_with(content)
+
+
+if __name__ == "__main__":
+    unittest.main()
